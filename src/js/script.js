6ec43386033374ba58333017416948e84c4dfe73
Agata Jarmoszuk <agnes365@vp.pl>
2021-12-13 20:30:50 +0100

    Add second exercise

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,8 @@
 {
   'use strict';
 
+  const favoriteBooks = [];
+
   function bookLoop() {
     const books = dataSource.books;
     const template = Handlebars.compile(document.querySelector('#template-book').innerHTML);
@@ -8,9 +10,23 @@
     for (const book of books) {
       const generatedHTML = template(book);
       const item = utils.createDOMFromHTML(generatedHTML);
+      item.addEventListener('dblclick', function (event) {
+        event.preventDefault();
+        const link = item.querySelector('.book__image');
+        const id = link.getAttribute('data-id')
+        if (!favoriteBooks.includes(id))
+          favoriteBooks.push(id);
+        else {
+          const index = favoriteBooks.indexOf(id);
+          favoriteBooks.splice(index, 1);
+        }
+        console.log("tablica", favoriteBooks);
+        link.classList.toggle('favorite');
+      });
       container.appendChild(item);
     }
   }
 
   bookLoop();
+
 }

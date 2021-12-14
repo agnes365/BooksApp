a7e65430c29174b8ef1b9df9a13f782baa1ece00
Agata Jarmoszuk <agnes365@vp.pl>
2021-12-14 23:07:17 +0100

    Add sixth exercise

--- src/js/script.js
+++ src/js/script.js
@@ -12,7 +12,25 @@
     const template = Handlebars.compile(document.querySelector('#template-book').innerHTML);
     const container = document.querySelector(booksWrapper);
     for (const book of books) {
-      const generatedHTML = template(book);
+      const templateBook = {};
+      templateBook.id = book.id;
+      templateBook.name = book.name;
+      templateBook.price = book.price;
+      templateBook.rating = book.rating;
+      templateBook.image = book.image;
+      templateBook.ratingWidth = book.rating * 10;
+
+      let ratingBgc = 'linear-gradient(to bottom, #fefcea 0%, #f1da36 100%);';
+      if (book.rating > 6 && book.rating <= 8)
+        ratingBgc = 'linear-gradient(to bottom, #b4df5b 0%, #b4df5b 100%);';
+      else if (book.rating > 8 && book.rating <= 9)
+        ratingBgc = 'linear-gradient(to bottom, #299a0b 0%, #299a0b 100%);';
+      else if (book.rating > 9)
+        ratingBgc = 'linear-gradient(to bottom, #ff0084 0%, #ff0084 100%);';
+
+      templateBook.ratingBgc = ratingBgc;
+
+      const generatedHTML = template(templateBook);
       const item = utils.createDOMFromHTML(generatedHTML);
       container.appendChild(item);
     }
